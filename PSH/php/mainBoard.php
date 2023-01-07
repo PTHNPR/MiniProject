5c02f524ef4793557b0b6b786d39db1c01461051
tngjs3327 <psheon9714@gmail.com>
2023-01-07 13:10:52 +0900

    html파일 -> php파일로 변환

---
+++ PSH/php/mainBoard.php
@@ -0,0 +1,20 @@
+<?php
+// require_once("connDB.php"); // DB연결 파일을 불러온다, (사용 후 mysqli_close($conn); 권장  : db와 연결을 끊는다)
+
+// $sql = 'select * from boardTable;';
+
+// $result = mysqli_query($conn, $sql);
+
+// $boardArr = mysqli_fetch_array($result);
+
+// $jArr = json_encode($boardArr);
+
+// // echo "<script>var jArr = $jArr;</script>";
+
+// echo "let boardArr1 = $jArr";
+
+// mysqli_close($conn);
+
+$var = 'Metallica';
+
+?>
